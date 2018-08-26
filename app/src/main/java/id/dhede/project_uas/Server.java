cae5671093d15018993817cc0486aae86f145072
dede40feet <dedepermana212@gmail.com>
2018-08-26 11:17:41 +0700

    test

---
+++ app/src/main/java/id/dhede/project_uas/Server.java
@@ -0,0 +1,8 @@
+package id.dhede.project_uas;
+/**
+ * Created by Dhede Permana Julianto on 20/8/2018.
+ */
+
+public class Server {
+    public static final String URL = "http://sasmitoh.nitarahmawati.my.id/";
+}
